58f8d27bea20ea51fdf067f8fe248e3350ba3595
HedwigNordlinder <160422581+HedwigNordlinder@users.noreply.github.com>
2024-06-26 13:35:58 +0200

    Added inference
    Inference performed using the analytical solution obtained from Dirichlet/Multionomial conjugacy

--- main.jl
+++ main.jl
@@ -1,7 +1,7 @@
-using Distributions, LinearAlgebra
+using Distributions, LinearAlgebra, Plots
 
-coin_count = 50
-toss_count = 10
+coin_count = 5000
+toss_count = 100
 
 function sample_true_distribution(n)
 
@@ -15,12 +15,35 @@ function sample_true_distribution(n)
 
 end
 
-biases = sample_true_distribution(50)
+biases = sample_true_distribution(coin_count)
 
-toss_matrix = Array{Int8}(undef,toss_count,coin_count)
+toss_matrix = Matrix(Array{Int8}(undef,toss_count,coin_count))
 
 for i in 1:toss_count
     for j in 1:coin_count
         toss_matrix[i,j] = rand() < biases[j] ? 1 : 0
      end
 end
+
+# We discretise our inference problem to 100 categories, 0, 0.01, 0.02 etc etc
+
+concentration_prior = 0.5 .* ones(100) # since we have 100 categories
+
+dirichlet_prior = rand(Dirichlet(concentration_prior))
+
+category_occurances = zeros(100)
+
+estimated_category = floor.(((Transpose(toss_matrix) * ones(toss_count)) ./ toss_count) .* 100) 
+
+for i in 1:coin_count
+
+    index = Integer(estimated_category[i])
+    if index > 0 # Kinda ugly but this works
+        category_occurances[index] += 1
+    end
+end
+
+concentration_posterior = (concentration_prior .+ category_occurances)
+dirichlet_posterior = rand(Dirichlet(concentration_posterior))
+prior_plot = plot(dirichlet_prior)
+posterior_plot = plot(dirichlet_posterior)
